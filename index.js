e90d5fe29075949558a338b87723c45d3fdaead5
abc <abc@abcs-MacBook-Pro.local>
2023-07-16 12:30:40 +0530

    all code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Backend code")
+const express = require('express')
+const morgan = require('morgan');
+const app = express()
+const port = 3000
+
+// body parser 
+
+app.use(express.json());
+app.use(morgan('default'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    console.log("get request is>>", req.body)
+    res.send(req.body)
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
